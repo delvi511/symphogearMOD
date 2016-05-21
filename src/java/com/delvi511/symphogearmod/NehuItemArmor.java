d38d988c4bb737ee5a66ac3a07eddb3247034609
Kory33 <konryoitdv@gmail.com>
2016-05-22 01:19:36 +0900

    refactored core and armor code

--- src/java/com/delvi511/symphogearmod/NehuItemArmor.java
+++ src/java/com/delvi511/symphogearmod/NehuItemArmor.java
@@ -12,28 +12,27 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 
-import org.lwjgl.input.Keyboard;
-
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
 public class NehuItemArmor extends ItemArmor{
-	// コンストラクタ
-	public NehuItemArmor(int type){
-		super(SymphogearModCore.NEHUSHTAN, 0, type);
-		
-		//クリエイティブのタブを「戦闘」に指定
-		this.setCreativeTab(CreativeTabs.tabCombat);
-	}
-
 	//防具のテクスチャ
 	public static final String HELMET_TEXTURE = "symphogear:neh_layer_1";
 	public static final String CHESTPLATE_TEXTURE = "symphogear:neh_layer_1";
 	public static final String LEGGINGS_TEXTURE = "symphogear:neh_layer_2";
 	public static final String BOOTS_TEXTURE = "symphogear:neh_layer_1";
 
-    //renderIndexは以下でArmorTextureを指定する場合はどんな値でも良い。
-
+	/**
+	 * @constructor
+	 * @param type 
+	 */
+	public NehuItemArmor(int type){
+		super(SymphogearModCore.NEHUSHTAN, 0, type);
+		
+		//クリエイティブのタブを「戦闘」に指定
+		this.setCreativeTab(CreativeTabs.tabCombat);
+	}
+	
 	@SideOnly(Side.CLIENT)
 	public String getArmorTexture(ItemStack itemStack, Entity entity, int slot, String type){
 		if (this.armorType == 2){
@@ -44,20 +43,29 @@ public class NehuItemArmor extends ItemArmor{
 	}
 	
 	@Override
-	@SuppressWarnings("unchecked")
 	public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack){		
-		// もしプレーヤーがスニーク中かつ設定されたキーを押下しているなら
-		if (!world.isRemote && player.isSneaking() && Keyboard.isKeyDown(SymphogearModCore.keyBinding)){
-			try{
-				List<Entity> entityList = world.getEntitiesWithinAABBExcludingEntity(player, player.boundingBox.expand(50, 10, 50));
-				for(Entity entity : entityList){
-					if(entity.isEntityAlive() && !entity.isDead && (entity instanceof EntityMob || entity instanceof EntitySlime)){
-						entity.attackEntityFrom(DamageSource.causeMobDamage((EntityPlayer)player), 500);
-						itemStack.damageItem(this.getMaxItemUseDuration(itemStack), (EntityPlayer)player	);
-					}
-				}
-			}catch(Throwable e){
+		if (!world.isRemote && player.isSneaking() && SymphogearModCore.isPurgeKeyPressed()){
+			executeArmorPurge(world, player, itemStack);
+		}
+	}
+	
+	/**
+	 * アーマーパージを指定AABB内の敵性mobに対して実行します。
+	 * @param world
+	 * @param player
+	 * @param itemStack
+	 */
+	@SuppressWarnings("unchecked")
+	private void executeArmorPurge(World world, EntityPlayer player, ItemStack itemStack){
+		List<Entity> entityList = world.getEntitiesWithinAABBExcludingEntity(player, player.boundingBox.expand(50, 10, 50));
 
+		for(Entity entity : entityList){
+			boolean isEntityEnemy = entity instanceof EntityMob || entity instanceof EntitySlime;
+
+			if(entity.isEntityAlive() && !entity.isDead && isEntityEnemy){
+				// mobにダメージを与え、防具を破壊
+				entity.attackEntityFrom(DamageSource.causeMobDamage((EntityPlayer)player), 500);
+				itemStack.damageItem(this.getMaxItemUseDuration(itemStack), (EntityPlayer)player);
 			}
 		}
 	}
