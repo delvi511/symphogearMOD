adb105a82763d4adea8da5bf009810358d05f243
Kory33 <konryoitdv@gmail.com>
2016-05-30 15:54:58 +0900

    extended ArmorProjectile from Entity

--- java/com/delvi511/symphogearmod/ArmorProjectile.java
+++ java/com/delvi511/symphogearmod/ArmorProjectile.java
@@ -1,14 +1,21 @@
 package com.delvi511.symphogearmod;
 
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.IProjectile;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.nbt.NBTTagCompound;
 
-public class ArmorProjectile extends EntityArrow {
+public class ArmorProjectile extends Entity implements IProjectile {
 	private double motionPerTick;
+	private EntityPlayer shootingEntity;
 	
 	public ArmorProjectile(EntityPlayer shooter, double yOffset, float pitch, float yaw, double speed){
-		super(shooter.getEntityWorld(), shooter.posX, shooter.posY + yOffset, shooter.posZ);
+		super(shooter.getEntityWorld());
 		this.shootingEntity = shooter;
+		
+		this.posX = shooter.posX;
+		this.posY = shooter.posY + yOffset;
+		this.posZ = shooter.posZ;
 		
 		this.motionPerTick = speed;
 		this.prevRotationPitch = this.rotationPitch = pitch;
@@ -22,5 +29,29 @@ public class ArmorProjectile extends EntityArrow {
 		this.prevPosX = this.motionX = Math.sin(this.rotationYaw / 180 * Math.PI)   * this.motionPerTick * xzFactor;
 		this.prevPosZ = this.motionZ = -Math.cos(this.rotationYaw / 180 * Math.PI)  * this.motionPerTick * xzFactor;
 		this.prevPosY = this.motionY = Math.cos(this.rotationPitch / 180 * Math.PI) * this.motionPerTick;
+	}
+
+	@Override
+	public void setThrowableHeading(double p_70186_1_, double p_70186_3_, double p_70186_5_, float p_70186_7_, float p_70186_8_) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	protected void entityInit() {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	protected void readEntityFromNBT(NBTTagCompound p_70037_1_) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+
+	@Override
+	protected void writeEntityToNBT(NBTTagCompound p_70014_1_) {
+		// TODO 自動生成されたメソッド・スタブ
+		
 	}
 }
