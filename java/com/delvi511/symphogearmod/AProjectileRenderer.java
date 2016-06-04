9cb3eaa3423b1d9bcec0e06db0275150ea89df70
Kory33 <konryoitdv@gmail.com>
2016-06-04 14:47:06 +0700

    Added bridge function to the renderer

--- java/com/delvi511/symphogearmod/AProjectileRenderer.java
+++ java/com/delvi511/symphogearmod/AProjectileRenderer.java
@@ -16,8 +16,16 @@ public class AProjectileRenderer extends Render{
 		this.shadowSize = 0.0f;
 	}
 	
-	@Override
-	public void doRender(Entity entity, double x, double y, double z, float yaw, float renderTick) {
+	/**
+	 * 実際にレンダー処理を行う関数
+	 * @param entity
+	 * @param x
+	 * @param y
+	 * @param z
+	 * @param yaw
+	 * @param renderTick
+	 */
+	public void doRender(ArmorProjectile entity, double x, double y, double z, float yaw, float renderTick) {
 		// 開始処理
 		GL11.glPushMatrix();
 		GL11.glDisable(GL11.GL_LIGHTING);
@@ -36,4 +44,11 @@ public class AProjectileRenderer extends Render{
 		return null;
 	}
 
+	/**
+	 * 引数をキャストしてから実際のレンダー処理関数を呼び出す関数。
+	 */
+	@Override
+	public void doRender(Entity entity, double x, double y, double z, float yaw, float renderTick){
+		this.doRender((ArmorProjectile)entity, x, y, z, yaw, renderTick);
+	}
 }
