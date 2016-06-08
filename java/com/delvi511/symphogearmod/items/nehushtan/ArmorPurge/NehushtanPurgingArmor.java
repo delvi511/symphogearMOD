47c18386250f7bcac68b83dce1012c6963e43170
Kory33 <konryoitdv@gmail.com>
2016-06-09 00:22:19 +0530

    added moved files

---
+++ java/com/delvi511/symphogearmod/items/nehushtan/ArmorPurge/NehushtanPurgingArmor.java
@@ -0,0 +1,36 @@
+package com.delvi511.symphogearmod.items.nehushtan.ArmorPurge;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+
+public class NehushtanPurgingArmor extends ItemArmor{
+	public static final String HELMET_TEXTURE = "symphogear:neh_pg_layer_1";
+	public static final String CHESTPLATE_TEXTURE = "symphogear:neh_pg_layer_1";
+	public static final String LEGGINGS_TEXTURE = "symphogear:neh_pg_layer_2";
+	public static final String BOOTS_TEXTURE = "symphogear:neh_pg_layer_1";
+
+	public NehushtanPurgingArmor(ArmorMaterial material, int type){
+		super(material, 0, type);
+		this.setCreativeTab(null);
+	}
+	
+	/*
+	 * TODO by Kory33
+	 * 
+	 * 防具をインベントリから外せないようにする。
+	 * 防具がダメージを受けないようにする。
+	 */
+	
+	@SideOnly(Side.CLIENT)
+	public String getArmorTexture(ItemStack itemStack, Entity entity, int slot, String type){
+		if (this.armorType == 2){
+			return "symphogear:textures/models/armor/neh_pg_layer_2.png";
+		}else{
+			return "symphogear:textures/models/armor/neh_pg_layer_1.png";
+		}
+	}
+
+}
