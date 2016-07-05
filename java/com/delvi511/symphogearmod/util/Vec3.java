a502c53043db5d851561f4782c306b844e71ed63
Kory33 <konryoitdv@gmail.com>
2016-07-06 00:07:26 +0530

    fixed vec3 issue, registored renderer

--- java/com/delvi511/symphogearmod/util/Vec3.java
+++ java/com/delvi511/symphogearmod/util/Vec3.java
@@ -9,24 +9,28 @@ public class Vec3 extends net.minecraft.util.Vec3 {
 		super(x, y, z);
 	}
 	
+	private static Vec3 castFromSuper(net.minecraft.util.Vec3 vec){
+		return new Vec3(vec.xCoord, vec.yCoord, vec.zCoord);
+	}
+	
 	public Vec3 subtract(Vec3 vec){
-		return (Vec3)super.subtract(vec);
+		return castFromSuper(super.subtract(vec));
 	}
 	
 	public Vec3 normalize(){
-		return (Vec3)super.normalize();
+		return castFromSuper(super.normalize());
 	}
 	
 	public Vec3 crossProduct(Vec3 vec){
-		return (Vec3)super.crossProduct(vec);
+		return castFromSuper(super.crossProduct(vec));
 	}
 	
 	public Vec3 addVector(double x, double y, double z){
-		return (Vec3)super.addVector(x, y, z);
+		return castFromSuper(super.addVector(x, y, z));
 	}
 	
 	public Vec3 add(Vec3 vec){
-		return (Vec3)super.addVector(vec.xCoord, vec.yCoord, vec.zCoord);
+		return castFromSuper(super.addVector(vec.xCoord, vec.yCoord, vec.zCoord));
 	}
 	
 	public Vec3 multiply(double f){
@@ -34,14 +38,14 @@ public class Vec3 extends net.minecraft.util.Vec3 {
 	}
 	
 	public Vec3 getIntermediateWithXValue(Vec3 vec, double x){
-		return (Vec3)super.getIntermediateWithXValue(vec, x);
+		return castFromSuper(super.getIntermediateWithXValue(vec, x));
 	}
 	
 	public Vec3 getIntermediateWithYValue(Vec3 vec, double y){
-		return (Vec3)super.getIntermediateWithYValue(vec, y);
+		return castFromSuper(super.getIntermediateWithYValue(vec, y));
 	}
 	
 	public Vec3 getIntermediateWithZValue(Vec3 vec, double z){
-		return (Vec3)super.getIntermediateWithZValue(vec, z);
+		return castFromSuper(super.getIntermediateWithZValue(vec, z));
 	}	
 }
